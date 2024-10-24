cece7160063da467efd32910dbf14b09a953abdf
bijomathewjose <bijomathewjose@gmail.com>
2024-10-24 17:20:28 +0530

    update : folder handling

--- routes/lifestyle_shots/lifestyle_shots.py
+++ routes/lifestyle_shots/lifestyle_shots.py
@@ -18,6 +18,17 @@ def sort_filenames(filenames):
     sorted_filenames = sorted(filenames, key=extract_number)
     return sorted_filenames
 
+def get_raw_images(sku_id,folder_path):
+    filenames=[f"{sku_id}_raw_{i}" for i in range(1,5)]
+    files=DIR.list_files_in_directory(folder_path)
+    image_files=[file for file in files if os.path.splitext(file)[1].lower() in IMAGE_EXTENSIONS]
+    required_files=[]
+    for file_name in image_files:
+        if any(file_name.startswith(path) for path in filenames):
+            required_files.append(file_name)
+    sorted_files=sort_filenames(required_files)
+    return sorted_files
+    
 def lifestyle_shots(user_id,sku_id):
     path=f"./assets/batch_process_output/{user_id}/{sku_id}/raw"
     logger.info(f"Starting lifestyle shots for SKU: {sku_id}")
@@ -25,17 +36,15 @@ def lifestyle_shots(user_id,sku_id):
         raise ValueError(f"Folder '{path}' does not exist.")
     else:
         logger.info(f"Folder '{path}' exists.")
-    files = DIR.list_files_in_directory(path)
-    images = [file for file in files if os.path.splitext(file)[1].lower() in IMAGE_EXTENSIONS]
+    images = get_raw_images(sku_id,path)
     if len(images) <=0:
         raise ValueError("No images found in the raw folder")
     count=0
-    sorted_images=sort_filenames(images)
     task_ids = []
     processed_folder = os.path.join(f"./assets/batch_process_output/{user_id}/{sku_id}/processed/lifestyle_shots")
     logger.info(f"Processed folder: {processed_folder}")
     os.makedirs(processed_folder, exist_ok=True)
-    for image in sorted_images:
+    for image in images:
         if count==4:
             break
         format=image.split('.')[-1]
