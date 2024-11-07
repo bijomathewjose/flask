d6ed31844f38696ab56fa2661d91803aa2bfdc85
bijomathewjose <bijomathewjose@gmail.com>
2024-11-07 22:53:47 +0530

    update : category addition

--- blueprints/creative_render/process_data.py
+++ blueprints/creative_render/process_data.py
@@ -5,13 +5,23 @@ def get_a_plus_template_models(data:List[List[str]],template_number:int):
     template_list=data[1:]
     template_index=template_number+1
     for template in template_list:
-        if 'A+' not in template[0].strip():
-            continue
+        category=template[0].strip()
+        name=template[1].strip()
         model=template[template_index]
-        if not model:
+        if model:
+            model=add_category_to_str(model,category,name)
+        else:
             continue
         models.append(model)
     return models
+
+def add_category_to_str(original_string:str,category_name:str,name:str):
+    position = original_string.find('{')
+    if position != -1:
+        new_string = original_string[:position + 1] + f'"category_name": "{category_name}","name": "{name}",' + original_string[position + 1:]
+    else:
+        new_string = original_string
+    return new_string
 
 def parse_text_lines(lines_dict):
     lines = []
@@ -28,35 +38,42 @@ def parse_text_lines(lines_dict):
     return {"data": lines, "no_of_lines": lines_dict.get("no_of_lines", None)}
 
 def segragate_images_and_text(data:Dict[str,Any]):
-    data= dict(eval(data))
-
-    images = []
-    texts = []
-    # Separate images and texts dynamically
-    for key, value in data.items():
-        if key.startswith("img"):
-            img_num = int(key[3])  # Extract the image number
-            while len(images) < img_num:
-                images.append({})
-            images[img_num - 1][key[5:]] = value  # Add property to correct image dict
-            images[img_num - 1]["number"] = img_num
-        elif key.startswith("text"):
-            text_num = int(key[4])  # Extract the text number
-            while len(texts) < text_num:
-                texts.append({})
-            if key.endswith("lines"):
-                texts[text_num - 1]["lines"] = parse_text_lines(value)
-            else:
-                texts[text_num - 1][key[6:]] = value
-            texts[text_num - 1]["number"] = text_num
-    return images, texts
+    try:
+        data= dict(eval(data))
+        name=data.get("name",None)
+        category=data.get("category_name",None)
+        images = []
+        texts = []
+        # Separate images and texts dynamically
+        for key, value in data.items():
+            if key.startswith("img"):
+                img_num = int(key[3])  # Extract the image number
+                while len(images) < img_num:
+                    images.append({})
+                images[img_num - 1][key[5:]] = value  # Add property to correct image dict
+                images[img_num - 1]["number"] = img_num
+            elif key.startswith("text"):
+                text_num = int(key[4])  # Extract the text number
+                while len(texts) < text_num:
+                    texts.append({})
+                if key.endswith("lines"):
+                    texts[text_num - 1]["lines"] = parse_text_lines(value)
+                else:
+                    texts[text_num - 1][key[6:]] = value
+                texts[text_num - 1]["number"] = text_num
+        return images, texts, name, category
+    except Exception as e:
+        print(f"Error: {data}")
+        raise e
+        
 
 def process_models(models:List[str]):
     list_of_text_types=[]
     for index,model in enumerate(models):
-        images,text_list=segragate_images_and_text(model)
+        images,text_list,name,category=segragate_images_and_text(model)
         list_of_text_types.append({
-            'demo':index+1,
+            'name':name,
+            'category':category,
             'images':images,
             'text_list':text_list   
         })
